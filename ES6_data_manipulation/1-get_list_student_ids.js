b137c24d3facadee7b9f51d35495faabdc25ff95
“kelciatkinson” <kelciatkinson@gmail.com>
2024-10-30 11:35:42 -0500

    linter

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,6 @@
-export default function getListStudentIds() {
-  if (Array.isArray(arguments[0])) {
-    return arguments[0].map((obj) => obj.id);
+export default function getListStudentIds(students) {
+  if (Array.isArray(students)) {
+    return students.map((obj) => obj.id);
   }
   return [];
 }
