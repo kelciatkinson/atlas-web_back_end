356f030775d41c333aaf31366e9f9c78650b64cd
“kelciatkinson” <kelciatkinson@gmail.com>
2024-10-30 09:05:38 -0500

    task 6

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,28 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(value) {
+    this._floors = value;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    this._sqft = value;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
+}
