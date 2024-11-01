1fdaf694d19b7d9c61e42a79c92b200a59078cd5
“kelciatkinson” <kelciatkinson@gmail.com>
2024-11-01 09:35:54 -0500

    update function

--- ES6_promise/7-load_balancer.js
+++ ES6_promise/7-load_balancer.js
@@ -1,10 +1,3 @@
 export default function loadBalancer(chinaDownload, USDownload) {
-  return new Promise((resolve) => {
-    const balancer = Math.random() < 0.5;
-    if (balancer) {
-      resolve({ load: chinaDownload });
-    } else {
-      resolve({ load: USDownload });
-    }
-  });
+  return Promise.race([chinaDownload, USDownload]);
 }
