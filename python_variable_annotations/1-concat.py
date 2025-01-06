6be3899c6b59468f59bab22674c0ecd1d16e92e9
“kelciatkinson” <kelciatkinson@gmail.com>
2025-01-06 17:06:19 -0600

    task 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Type-annotated function that takes two strings as arguments
+and returns a concatenated string.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Returns the concatenation of two strings."""
+    return str1 + str2
