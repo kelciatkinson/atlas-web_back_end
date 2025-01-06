508b3898dd8452bc339086702f4b165d0ca62aaf
“kelciatkinson” <kelciatkinson@gmail.com>
2025-01-06 16:58:17 -0600

    task 0

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""Type-annotated function that takes two floats as arguments and returns a float."""
+
+def add(a: float, b: float) -> float:
+    """Returns the sum of two floats as a float."""
+    return a + b
