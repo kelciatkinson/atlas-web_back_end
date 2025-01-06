6b310f85e32a42e372999e24685e678654c4a11d
“kelciatkinson” <kelciatkinson@gmail.com>
2025-01-06 17:50:24 -0600

    task 2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Type-annotated function that takes a float as an argument
+and returns the foor of the float.
+"""
+
+
+def floor(n: float) -> int:
+    """Returns the floor of a float."""
+    return int(n // 1)
