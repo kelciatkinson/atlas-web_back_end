f457013aef64d87120dba00010e8b665b5d98b17
“kelciatkinson” <kelciatkinson@gmail.com>
2025-01-06 17:53:03 -0600

    task 3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Type-annotated function that takes a float as an argument
+and returns the string representation of the float.
+"""
+
+
+def to_str(n: float) -> str:
+    """Returns the string representation of a float."""
+    return str(n)
