15d8a43269514bdcc742a9c4346daed6e3f9fc98
“kelciatkinson” <kelciatkinson@gmail.com>
2025-01-06 18:04:27 -0600

    update documentation

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-import typing
 """
 Type-annotated function that takes a list of floats
 as an argument and returns their sum as a float.
 """
+import typing
 
 
 def sum_list(input_list: typing.List[float]) -> float:
