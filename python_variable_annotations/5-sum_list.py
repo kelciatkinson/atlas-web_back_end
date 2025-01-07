beed70c584db50966a727d3d4d6760acf92f3817
“kelciatkinson” <kelciatkinson@gmail.com>
2025-01-06 18:01:50 -0600

    task 5

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import typing
+"""
+Type-annotated function that takes a list of floats
+as an argument and returns their sum as a float.
+"""
+
+
+def sum_list(input_list: typing.List[float]) -> float:
+    """Returns the sum of a list of floats."""
+    return sum(input_list)
