9e1a718f01112e35a83f3ce2e14d412aa40a7688
“kelciatkinson” <kelciatkinson@gmail.com>
2025-01-09 14:12:17 -0600

    updated task 0 to return the delays without running await twice and task 1

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -13,5 +13,6 @@ async def wait_random(max_delay: int = 10) -> float:
     and waits for a random delay between 0 and max_delay and
     eventually returns it.
     """
-    await asyncio.sleep(random.uniform(0, max_delay))
-    return random.uniform(0, max_delay)
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Dynmically import previous python file and asynchronous routine
+that taks two arguments that will spay wait_random n times with
+specified max_delay.
+"""
+import asyncio
+import importlib.util
+
+
+module_name = "wait_random"
+file_path = "0-basic_async_syntax.py"
+
+spec = importlib.util.spec_from_file_location(module_name, file_path)
+module = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(module)
+
+wait_random = module.wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> float:
+    """
+    Return the list of all of the delays in float values
+    """
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    delays = await asyncio.gather(*tasks)
+    return sorted(delays)
