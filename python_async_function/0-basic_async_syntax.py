747cebe961283261d607beb6aad43616a10fc860
“kelciatkinson” <kelciatkinson@gmail.com>
2025-01-09 11:27:28 -0600

    update return annotation

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -6,7 +6,7 @@ Defines an aynchronous coroutine.
 """
 
 
-async def wait_random(max_delay: int = 10):
+async def wait_random(max_delay: int = 10) -> float:
     """
     Asynchronous coroutine that takes in an integer argument
     and waits for a random delay between 0 and max_delay and
