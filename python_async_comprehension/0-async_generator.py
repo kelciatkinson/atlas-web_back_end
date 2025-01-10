db05999845a2db884824d756288bd8636dbafc24
“kelciatkinson” <kelciatkinson@gmail.com>
2025-01-10 15:05:17 -0600

    documentation and annotation

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 """
-
+Coroutine that takes no argumens and yields 10 random numbers.
 """
 import asyncio
 import random
 
 
-async def async_generator():
+async def async_generator() -> float:
     """
+    Yields 10 random floats between the numbers 0-10.
     """
     for i in range(10):
         await asyncio.sleep(1)
