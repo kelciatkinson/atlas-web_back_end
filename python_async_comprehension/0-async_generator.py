d618d51507f2492eacaea9d58a5279b55a9ad7bb
“kelciatkinson” <kelciatkinson@gmail.com>
2025-01-10 15:38:57 -0600

    updated annotation

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -4,9 +4,10 @@ Coroutine that takes no argumens and yields 10 random numbers.
 """
 import asyncio
 import random
+import typing
 
 
-async def async_generator() -> float:
+async def async_generator() -> typing.Generator[float, None, None]:
     """
     Yields 10 random floats between the numbers 0-10.
     """
