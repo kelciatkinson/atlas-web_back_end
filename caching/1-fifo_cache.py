d14c3f8abb5e1a700edb42c24bb253d7dd0621a6
“kelciatkinson” <kelciatkinson@gmail.com>
2025-01-13 12:25:32 -0600

    task 1

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -1,4 +1,35 @@
 #!/usr/bin/env python3
 """
+Caching system inheriting from base_caching.
 """
+from base_caching import BaseCaching
 
+
+class FIFOCache(BaseCaching):
+    """
+    Implements a caching system that
+    stores and retrieves items from caache.
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        Method to store items in cache.
+        """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first_key = next(iter(self.cache_data))
+            del self.cache_data[first_key]
+            print(f"DISCARD: {first_key}")
+
+    def get(self, key):
+        """
+        Method to retrieve items from cache.
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
