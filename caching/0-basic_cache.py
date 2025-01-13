9c8ae32588bf778fc814a80224ecef3d56dc236d
“kelciatkinson” <kelciatkinson@gmail.com>
2025-01-13 12:02:20 -0600

    task 1 and file creation

---
+++ caching/0-basic_cache.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Caching system inheriting from base_caching.
+"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    Implements a basic caching system that
+    stores and retrieves items from caache. 
+    """
+
+    def put(self, key, item):
+        """
+        Method to store items in cache.
+        """
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Method to retrieve items from cache.
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data.get(key)
