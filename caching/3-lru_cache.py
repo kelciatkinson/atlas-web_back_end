a731b601a0be76f97ce19f68903debbe5a7967b7
“kelciatkinson” <kelciatkinson@gmail.com>
2025-01-14 15:46:22 -0600

    beginning of task 3

--- caching/3-lru_cache.py
+++ caching/3-lru_cache.py
@@ -1,4 +1,42 @@
 #!/usr/bin/env python3
 """
+Caching system inheriting from base_caching.
 """
+from base_caching import BaseCaching
 
+
+class LRUCache(BaseCaching):
+    """
+    Implements a caching system that
+    stores and retrieves items from caache.
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.queue = []
+
+    def put(self, key, item):
+        """
+        Adds an item to the cache.
+        """
+        if key is None or item is None:
+            return
+
+        self.cache_data[key] = item
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            discard = self.queue.pop(0)
+            print(f"DISCARD: {discard}")
+            del self.cache_data[discard]
+
+        self.queue.append(key)
+
+    def get(self, key):
+        """
+        Retrieves an item from the cache.
+        """
+        if key is None or key not in self.cache_data:
+            return 
+        self.queue.remove(key)
+        self.queue.append(key)
+        return self.cache_data[key]
