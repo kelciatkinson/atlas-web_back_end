1116f03fb859f542a7ce10f95bb846381cb5c578
“kelciatkinson” <kelciatkinson@gmail.com>
2025-01-16 11:30:34 -0600

    task 4

--- caching/4-mru_cache.py
+++ caching/4-mru_cache.py
@@ -1,4 +1,44 @@
 #!/usr/bin/env python3
 """
+Caching system inheriting from base_caching.
 """
+from base_caching import BaseCaching
 
+
+class MRUCache(BaseCaching):
+    """
+    Implements a caching system that
+    stores and retrieves items from caache.
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.queue = []
+
+    def put(self, key, item):
+        """
+        Adds an item to the cache.
+        """
+        if key is None or item is None:
+            return
+
+        if key in self.cache_data:
+            self.queue.remove(key)
+        self.cache_data[key] = item
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            discard = self.queue.pop()
+            print(f"DISCARD: {discard}")
+            del self.cache_data[discard]
+        self.cache_data[key] = item
+        self.queue.append(key)
+
+    def get(self, key):
+        """
+        Retrieves an item from the cache.
+        """
+        if key is None or key not in self.cache_data:
+            return
+        self.queue.remove(key)
+        self.queue.append(key)
+        return self.cache_data[key]
