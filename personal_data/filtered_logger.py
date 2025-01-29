0f2388ff1270f0b2a0c71a613d72d7555f358e9a
“kelciatkinson” <kelciatkinson@gmail.com>
2025-01-28 18:32:55 -0600

    task 3

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -5,6 +5,8 @@ Module for filtering sensitive info from log messages.
 import re
 from typing import List
 import logging
+import os
+import mysql.connector
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
@@ -34,6 +36,21 @@ def get_logger() -> logging.Logger:
     logger.addHandler(stream_handler)
 
     return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """Returns a connector to the database.
+    """
+    username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = os.getenv("PERSONAL_DATA_DB_NAME", "my_db")
+    return mysql.connector.connect(
+        host=host,
+        user=username,
+        password=password,
+        database=db_name
+    )
 
 
 class RedactingFormatter(logging.Formatter):
