989fcd22fc9ba6582301ca9887189d920747d162
“kelciatkinson” <kelciatkinson@gmail.com>
2025-01-29 15:36:30 -0600

    task 4

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -11,6 +11,11 @@ class Auth():
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Returns False - path and exlduded_paths will be used later.
         """
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        for excluded_path in excluded_paths:
+            if path == excluded_path:
+                return False
         return False
 
     def authorization_header(self, request=None) -> str:
