7aec3cd148b716a11e44a61ccee6226d20ea399a
“kelciatkinson” <kelciatkinson@gmail.com>
2025-01-29 19:42:47 -0600

    update documentation

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,6 +9,8 @@ class BasicAuth(Auth):
     """
     def extract_base64_authorization_header(self,
                                             authorization_header: str) -> str:
+        """Returns Base64 part of Authorization header
+        """
         if authorization_header is None:
             return None
 
