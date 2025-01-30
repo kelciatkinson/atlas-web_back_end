d96d14ed028017b6dedfba9eb22bc0eb51923018
“kelciatkinson” <kelciatkinson@gmail.com>
2025-01-29 20:53:51 -0600

    task 9

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -39,3 +39,22 @@ class BasicAuth(Auth):
             return decoded.decode('utf-8')
         except Exception:
             return None
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header: str) -> (
+                                 str, str):
+        """Returns user email and password from Bas64 decoded value.
+        """
+
+        if decoded_base64_authorization_header is None:
+            return (None, None)
+
+        if not isinstance(decoded_base64_authorization_header, str):
+            return (None, None)
+
+        if ':' not in decoded_base64_authorization_header:
+            return (None, None)
+
+        (user_email,
+         user_password) = decoded_base64_authorization_header.split(':', 1)
+        return (user_email, user_password)
