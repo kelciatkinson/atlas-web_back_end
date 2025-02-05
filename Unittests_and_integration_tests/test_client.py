42cd331ee7d02c78e2fe9d88abae8b39dc15219c
“kelciatkinson” <kelciatkinson@gmail.com>
2025-02-05 14:38:34 -0600

    task 5

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -3,7 +3,7 @@
 """
 import unittest
 from client import GithubOrgClient
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
 
 
@@ -17,10 +17,25 @@ class TestGithubOrgClient(unittest.TestCase):
     ])
     @patch('client.get_json')
     def test_org(self, org_url, mock_get_json):
-        """Tests GithubOrgClient.org returns the correct value
+        """TestsGithubOrgClient.org returns the correct value
         """
         test_client = GithubOrgClient(org_url)
         test_client.org
         mock_get_json.assert_called_once_with(
             GithubOrgClient.ORG_URL.format(org=org_url)
         )
+
+    @patch('client.GithubOrgClient.org')
+    def test_public_repos_url(self, mock_org):
+        """Test that the result of _public_repos_url is the expected one
+        based on the mocked payload
+        """
+        test_payload: dict = {"repos_url": "https://example.com/test"}
+        with patch.object(
+            GithubOrgClient, "org", new_callable=PropertyMock
+        ) as mock_org:
+            mock_org.return_value = test_payload
+            request = GithubOrgClient("test")
+            result = request._public_repos_url
+
+            self.assertEqual(result, test_payload["repos_url"])
