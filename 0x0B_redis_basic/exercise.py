e02629295c130e4daa824fbf693083e1f5393296
“kelciatkinson” <kelciatkinson@gmail.com>
2025-02-12 19:46:40 -0600

    task 1

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -4,7 +4,7 @@ database. It uses random key by UUID to store data.
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache():
@@ -22,3 +22,34 @@ class Cache():
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self,
+            key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes,
+                                                    int, float, None]:
+        """Retrieves data from Redis.
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> Union[str, None]:
+        """Retrieves data from Redis as a string.
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if data:
+            return data.decode('utf-8')
+        return None
+
+    def get_int(self, key: str) -> int:
+        """Retrieves data and converts into integer.
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        return self.get(key, fn=int)
