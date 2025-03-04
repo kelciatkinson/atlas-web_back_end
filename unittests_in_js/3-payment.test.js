cf7c3a2bc9bfca640640bb5a7a1e294188781521
“kelciatkinson” <kelciatkinson@gmail.com>
2025-03-04 17:49:00 -0600

    moved spy creation to beforeEach hook

--- unittests_in_js/3-payment.test.js
+++ unittests_in_js/3-payment.test.js
@@ -5,7 +5,11 @@ const Utils = require("./utils");
 const sendPaymentRequestToApi = require("./3-payment");
 
 describe("sendPaymentRequestToApi", function () {
-    const spy = sinon.spy(Utils, "calculateNumber");
+    let spy;
+
+    beforeEach(() => {
+        spy = sinon.spy(Utils, "calculateNumber");
+    });
 
     it("should call Utils.calculateNumber with 'SUM', 100, 20", function () {
         sendPaymentRequestToApi(100, 20);
@@ -13,5 +17,7 @@ describe("sendPaymentRequestToApi", function () {
         expect(spy.calledWithExactly("SUM", 100, 20)).to.be.true;
     });
 
-    spy.restore();
+    afterEach(() => {
+        spy.restore();
+    });
 });
