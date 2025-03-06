2599d15f33cfca09b93a055ea0755771ce87f20a
“kelciatkinson” <kelciatkinson@gmail.com>
2025-03-06 17:58:25 -0600

    tassk 6

---
+++ queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,23 @@
+import kue from 'kue';
+
+const queue = kue.createQueue();
+
+const jobData = {
+    phoneNumber: '123123123',
+    message: 'This is a job creator message'
+};
+
+const job = queue.create('push_notification_code', jobData)
+    .save((err) => {
+        if (!err) {
+            console.log(`Notification job created: ${job.id}`);
+        }
+    });
+
+job.on('complete', () => {
+    console.log('Notification job completed');
+});
+
+job.on('failed', () => {
+    console.log('Notification job failed');
+});
