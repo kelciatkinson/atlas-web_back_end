ac66c71501e41fd72ccd8e449be4ddede3e5bda9
“kelciatkinson” <kelciatkinson@gmail.com>
2025-03-05 23:31:46 -0600

    added regex to the endpoint for id

--- unittests_in_js/9-api/api.js
+++ unittests_in_js/9-api/api.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
     res.send("Welcome to the payment system");
 });
 
-app.get('/cart/:id', (req, res) => {
+app.get('/cart/:id(\d)', (req, res) => {
     const id = req.params.id;
     res.send(`Payment methods for cart ${id}`);
 });
